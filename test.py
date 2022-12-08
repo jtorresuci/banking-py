7b6ede6048366bbfa513f51672e84d22d02f00e3
Helen Truong <108304266+helen-truong@users.noreply.github.com>
2022-12-07 19:27:07 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,29 @@
 from bankAccount import BankAccount
 
-
+#Instantiate 3 objects
 adamsAcc = BankAccount(100, "Adam", "checking")
 samsAcc = BankAccount(100, "Sam", "saving")
 leilasAcc = BankAccount(500, "Leila", "checking")
 
+# adding interest to all three accounts
 adamsAcc.addInterest()
 leilasAcc.addInterest()
 samsAcc.addInterest()
+
+# using getBalance to print
+print(adamsAcc.getBalance())
+print(samsAcc.getBalance())
+print(leilasAcc.getBalance())
+
+# testing exception with transfer more than balance amount
+adamsAcc.withdraw(101)
+samsAcc.withdraw(101)
+leilasAcc.withdraw(101)
+
+# testing transfer and getName/getBalance
+samsAcc.transfer(200, adamsAcc.getName())
+leilasAcc.transfer(50, adamsAcc.getName())
+print(adamsAcc.getBalance())
+print(leilasAcc.getBalance())
 
 print("Leila's balance after interest is $%0.2f" % (leilasAcc.getBalance()))
